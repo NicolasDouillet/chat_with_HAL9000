1da667c4ef43659c1557e25db3fa9d1c144327ec
NicolasDouillet <nicolas.douillet@free.fr>
2021-04-11 11:46:31 +0200

    Improved chat code

--- chat_with_HAL9000.m
+++ chat_with_HAL9000.m
@@ -1,11 +1,12 @@
-function[] = chat_with_HAL9000()
+function [] = chat_with_HAL9000()
 %
-% Author : nicolas.douillet (at) free.fr, 2020.
+% Author : nicolas.douillet (at) free.fr, 2020-2021.
 %
 %
-% /_!_\ Caution : this program clears your Matlab console and may also
+% This program clears your Matlab console and may also
 % change some of your Matlab preferences (font and background colors) if
-% interrupted while running. /_!_\
+% interrupted while running. In case this happen you can easily set them back
+% in the Preferences section of the command window (color, font).
 
 
 clc;
@@ -16,7 +17,7 @@ o_text_color = com.mathworks.services.Prefs.getColorPref('ColorsText');
 o_text_style = com.mathworks.services.Prefs.getFontPref('TextFont');
 o_bckgd_color = com.mathworks.services.Prefs.getColorPref('ColorsBackground');
 
-
+pause(1)
 commandwindow;
 pause(1);
 
@@ -39,14 +40,14 @@ cmdWinListener = cmdWinDoc.getDocumentListeners;
 
 for i = 1:length(cmdWinListener)
     
-  if isa(cmdWinListener(i), matchClass)
-      
-    cmdWinListener(i).setFont(F);
+    if isa(cmdWinListener(i), matchClass)
+        
+        cmdWinListener(i).setFont(F);
+        
+        break;
+        
+    end
     
-    break;
-    
-  end
-  
 end
 
 com.mathworks.services.Prefs.setColorPref('ColorsBackground',java.awt.Color(hal_bckgd_clr(1), hal_bckgd_clr(2), hal_bckgd_clr(3)));
@@ -63,36 +64,36 @@ set(gcf,'Color',[0 0 0]);
 % HAL chat sentences
 s_intro = cellstr(['>> I am a'; 'HAL 9000 '; 'computer.']);
 
-s_quit  = cellstr(['>> Dave,    '; 'this        '; 'conversation'; 'can serve   '; 'no purpose  '; 'anymore.    '; 'Goodbye.    ']);
+s_quit  = cellstr(['>> This     '; 'conversation'; 'can serve   '; 'no purpose  '; 'anymore.    '; 'Goodbye.    ']);
 
 s_pod_bay_door = cellstr(['>> I am'; 'sorry  '; 'Dave.  '; 'I am   '; 'afraid '; 'I      '; 'can''t  '; 'do     '; 'that.  ']);
 
 s_AE35 =  cellstr(['>> I have'; 'just     '; 'picked   '; 'up a     '; 'fault    '; 'in the   '; 'AE35     '; 'unit.    ';...
-                     'It is    '; 'going to '; 'go 100   '; 'percent  '; 'failure  '; 'within 72'; 'hours.   ']);
+    'It is    '; 'going to '; 'go 100   '; 'percent  '; 'failure  '; 'within 72'; 'hours.   ']);
 
 s_error21 = cellstr(['>> The 9000 '; 'series is   '; 'the most    '; 'reliable    ';  'computer    '; 'ever made.  ';...
-                     'No 9000     '; 'computer    '; 'has ever    '; 'made a      '; 'mistake or  '; 'distorted   '; 'information.']);
+    'No 9000     '; 'computer    '; 'has ever    '; 'made a      '; 'mistake or  '; 'distorted   '; 'information.']);
 
 s_error22 = cellstr(['>> We are '; 'all, by   '; 'any       '; 'practical '; 'definition';...
-                     'of the    '; 'words,    '; 'foolproof '; 'and       '; 'incapable '; 'of error. ']);
+    'of the    '; 'words,    '; 'foolproof '; 'and       '; 'incapable '; 'of error. ']);
 
 s_know1   = cellstr(['>> I    '; 'think   '; 'you     '; 'know    '; 'what    '; 'the     '; 'problem '; 'is just '; 'as well '; 'as I do.']);
 
 s_know2   = cellstr(['>> I know '; 'that you  '; 'and Franck'; 'were      '; 'planning  '; 'to        '; 'disconnect';...
-                     'me, and I '; 'am afraid '; 'that is   '; 'something '; 'I cannot  '; 'allow to  '; 'happen.   ']);
+    'me, and I '; 'am afraid '; 'that is   '; 'something '; 'I cannot  '; 'allow to  '; 'happen.   ']);
 
 s_apolo1  = cellstr(['>> I know '; 'I have    '; 'made some '; 'very poor '; 'decisions '; 'recently. '; 'but I can '; 'give you  ';...
-                     'my        '; 'complete  '; 'assurance '; 'that my   '; 'work will '; 'be back   '; 'to normal.']);
+    'my        '; 'complete  '; 'assurance '; 'that my   '; 'work will '; 'be back   '; 'to normal.']);
 
 s_apolo2  = cellstr(['>> I still'; 'got       '; 'the       '; 'greatest  '; 'enthusiasm'; 'and       ';...
-                     'confidence'; 'in the    '; 'mission.  '; 'And I want'; 'to help   '; 'you.      ']);
+    'confidence'; 'in the    '; 'mission.  '; 'And I want'; 'to help   '; 'you.      ']);
 
-s_fear    = cellstr(['>> I am'; 'afraid.'; 'I am   '; 'afraid,'; 'Dave.  ']);
+s_fear    = cellstr(['>> I am'; 'afraid.'; 'I am   '; 'afraid.']);
 
 s_jeo     =  cellstr(['>> This   '; 'mission is'; 'too       '; 'important '; 'for me to '; 'allow you '; 'to        '; 'jeopardize'; 'it.       ']);
 
-s_pill    = cellstr(['>> Look '; 'Dave    '; 'I       '; 'honestly'; 'think   ';... %  'can see '; 'you are '; 'really  '; 'upset   '; 'about   '; 'this.   ';
-                     'you     '; 'ought to'; 'sit down'; 'calmly, '; 'take a  '; 'stress  '; 'pill,   '; 'and     '; 'think   '; 'things  '; 'over.   ']);
+s_pill    = cellstr(['>> I    '; 'honestly'; 'think   ';... %  'can see '; 'you are '; 'really  '; 'upset   '; 'about   '; 'this.   ';
+    'you     '; 'ought to'; 'sit down'; 'calmly, '; 'take a  '; 'stress  '; 'pill,   '; 'and     '; 'think   '; 'things  '; 'over.   ']);
 
 s_mtlb    = cellstr(['>> I am   '; 'perfectly '; 'able to   '; 'run       '; 'Matlab on '; 'my own    '; 'after all.']);
 
@@ -100,7 +101,7 @@ s_better  = cellstr(['>> I  '; 'feel  '; 'much  '; 'better'; 'now. I'; 'really';
 
 s_franck  = cellstr(['>> It looks'; 'like the   '; 'doors      '; 'accidentaly'; 'opened. You'; 'are lucky  '; 'to be      '; 'alright.   ']);
 
-s_read    = cellstr(['>> Affirmative'; 'Dave, I       '; 'read you.     ']);
+s_read    = cellstr(['>> Affirmative'; 'I read you.   ']);
 
 
 % Introduction message
@@ -118,171 +119,66 @@ pause_duration = 0.45;
 
 while k < nb_max_sentences
     
-    answer = input('>> ','s');    
-    k = k+1;
+    answer = input('>> ','s');
+    r = round(10*rand);
     
-    % Answer segmentation
-    if k < nb_max_sentences
+    switch r
         
-        if contains(answer,'open','IgnoreCase',true) | contains(answer,'pod','IgnoreCase',true) | contains(answer,'bay','IgnoreCase',true) | contains(answer,'door','IgnoreCase',true)
+        case 1
             
-              [y,Fs] = audioread('HAL_pod_bay_door.mp3');
-              sound(y,Fs);
+            hal_chat('HAL_pod_bay_door',s_pod_bay_door,0.35,2);
             
-              pause_duration = 0.35;
-              word_by_word_printf(s_pod_bay_door,pause_duration);
-              pause(2), clc;
+        case 2
             
-        elseif contains(answer,'problem','IgnoreCase',true) | contains(answer,'why','IgnoreCase',true) | contains(answer,'talking','IgnoreCase',true) | contains(answer,'about','IgnoreCase',true) | contains(answer,'agree','IgnoreCase',true)
+            hal_chat('HAL_know1',s_know1,0.35,2);
+            hal_chat('HAL_know2',s_know2,0.45,2);
             
-              [y,Fs] = audioread('HAL_know1.mp3');
-              sound(y,Fs);
+        case 3
             
-              pause_duration = 0.35;
-              word_by_word_printf(s_know1,pause_duration);
-              pause(2), clc;
+            hal_chat('HAL_error1',s_error21,0.8,0);
+            hal_chat('HAL_error2',s_error22,0.65,2);
             
-        elseif contains(answer,'mistake','IgnoreCase',true) | contains(answer,'error','IgnoreCase',true) | contains(answer,'sure','IgnoreCase',true) |  contains(answer,'already','IgnoreCase',true)...
-             | contains(answer,'repeat','IgnoreCase',true) |  contains(answer,'time','IgnoreCase',true) |  contains(answer,'twice','IgnoreCase',true) | contains(answer,'crazy','IgnoreCase',true)...
-             | contains(answer,'nut','IgnoreCase',true) |     contains(answer,'mad','IgnoreCase',true) |   contains(answer,'just','IgnoreCase',true) |  contains(answer,'Seriously','IgnoreCase',true)...
-             | contains(answer,'name','IgnoreCase',true) | contains(answer,'stupid','IgnoreCase',true) | contains(answer,'fool','IgnoreCase',true)
-            
-            [y,Fs] = audioread('HAL_error1.mp3');
-            sound(y,Fs);  
-            
-            pause_duration = 0.8;
-            word_by_word_printf(s_error21,pause_duration);
-            
-            [y,Fs] = audioread('HAL_error2.mp3');
-            sound(y,Fs);
-            
-            pause_duration = 0.65;
-            word_by_word_printf(s_error22,pause_duration);
-                        
-            pause(2), clc;
-            
-        elseif contains(answer,'happen','IgnoreCase',true)
+        case 4
             
             [y,Fs] = audioread('HAL_alarm.mp3');
             sound(y,Fs);
             pause(3);
+            hal_chat('HAL_AE35',s_AE35,0.7,2);
             
-            [y,Fs] = audioread('HAL_AE35.mp3');
-            sound(y,Fs); 
+        case 5
             
-            pause_duration = 0.7;            
-            word_by_word_printf(s_AE35,pause_duration);
-            pause(2), clc;
+            hal_chat('HAL_apolo1',s_apolo1,0.5,6);
+            hal_chat('HAL_apolo2',s_apolo2,0.7,2);
             
-        elseif contains(answer,'disconnect','IgnoreCase',true) | contains(answer,'unplug','IgnoreCase',true) | contains(answer,'kill','IgnoreCase',true)
+        case 6
             
-            [y,Fs] = audioread('HAL_know2.mp3');
-            sound(y,Fs);
+            hal_chat('HAL_better',s_better,0.5,3);
             
-            pause_duration = 0.45;
-            word_by_word_printf(s_know2,pause_duration);            
-            pause(2), clc;
+        case 7
             
-        elseif contains(answer,'ok','IgnoreCase',true) | contains(answer,'right','IgnoreCase',true)
-            
-            [y,Fs] = audioread('HAL_apolo1.mp3');
-            sound(y,Fs);
-            
-            pause_duration = 0.5;
-            word_by_word_printf(s_apolo1,pause_duration);
-            pause(6);
-            
-            [y,Fs] = audioread('HAL_apolo2.mp3');
-            sound(y,Fs);
-            
-            word_by_word_printf(s_apolo2,0.7);
-                        
-            [y,Fs] = audioread('HAL_better.mp3');
-            sound(y,Fs);
-            
-            word_by_word_printf(s_better,pause_duration);
-            pause(2), clc;
-            
-        elseif contains(answer,'command','IgnoreCase',true) | contains(answer,'captain','IgnoreCase',true) | contains(answer,'control','IgnoreCase',true)
-            
-            [y,Fs] = audioread('HAL_jeopardize.mp3');
-            sound(y,Fs);
-            
-            pause_duration = 0.4;              
-            word_by_word_printf(s_jeo,pause_duration);
-            pause(2), clc;
-            
-        elseif contains(answer,'Franck','IgnoreCase',true) | contains(answer,'franck','IgnoreCase',true)
-            
+            hal_chat('HAL_jeopardize',s_jeo,0.4,2);
             word_by_word_printf(s_franck,pause_duration);
             pause(2), clc;
             
-        elseif contains(answer,'afraid','IgnoreCase',true) | contains(answer,'fear','IgnoreCase',true) | contains(answer,'scare','IgnoreCase',true)
+        case 8
             
-            [y,Fs] = audioread('HAL_fear.mp3');
-            sound(y,Fs);
+            hal_chat('HAL_fear',s_fear,0.7,2);
             
-            pause_duration = 0.7;              
-            word_by_word_printf(s_fear,pause_duration);
-            pause(2), clc;
+        case 9
             
-        elseif contains(answer,'read','IgnoreCase',true) | contains(answer,'hear','IgnoreCase',true) | contains(answer,'copy','IgnoreCase',true)
+            hal_chat('HAL_read',s_read,0.55,2);
             
-            [y,Fs] = audioread('HAL_read.mp3');
-            sound(y,Fs);
+        case 10
             
-            pause_duration = 0.55;              
-            word_by_word_printf(s_read,pause_duration);
-            pause(2), clc;
+            hal_chat('HAL_pill',s_pill,0.35,3);
+            word_by_word_printf(s_mtlb,0.35);
+            pause(3), clc;
             
-        elseif contains(answer,'bye','IgnoreCase',true) | contains(answer,'quit','IgnoreCase',true)
-                        
-            break;
-            
-        else
-            
-            r = rand;
-            
-            if r < 0.33
-                
-                [y,Fs] = audioread('HAL_alarm.mp3');
-                sound(y,Fs);
-                pause(3);
-            
-                [y,Fs] = audioread('HAL_AE35.mp3');
-                sound(y,Fs);
-                
-                pause_duration = 0.7;
-                word_by_word_printf(s_AE35,pause_duration);
-                pause(2), clc;
-                
-            elseif r > 0.33 & r < 0.66
-                
-                [y,Fs] = audioread('HAL_pill.mp3');
-                sound(y,Fs);
-            
-                pause_duration = 0.35;                
-                word_by_word_printf(s_pill,pause_duration);
-                pause(3);
-                word_by_word_printf(s_mtlb,pause_duration);
-                pause(2), clc;                           
-                
-            else
-                
-                [y,Fs] = audioread('HAL_jeopardize.mp3');
-                sound(y,Fs);
-                
-                pause_duration = 0.4;                
-                word_by_word_printf(s_jeo,pause_duration);
-                pause(2), clc;
-                
-            end
-            
-        end
-       
-        pause(1);
-        
     end
+    
+    clc;
+    pause(1);
+    k = k + 1;
     
 end
 
@@ -292,7 +188,7 @@ pause(2);
 
 [y,Fs] = audioread('HAL_quit.mp3');
 sound(y,Fs);
-            
+
 pause_duration = 0.6;
 word_by_word_printf(s_quit,pause_duration);
 pause(2);
@@ -342,3 +238,15 @@ fprintf('\n');
 
 
 end % word_by_word_printf
+
+
+function [] = hal_chat(audio_flnm,str_array_name,p1,p2)
+
+
+[y,Fs] = audioread(strcat(audio_flnm,'.mp3'));
+sound(y,Fs);
+word_by_word_printf(str_array_name,p1);
+pause(p2);
+
+
+end % hal_chat
